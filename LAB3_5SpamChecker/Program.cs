5be902fe32f7e077360d1cc68192da4f079b849f
310lbenson@gmail.com <310lbenson@gmail.com>
2020-06-23 11:59:10 -0400

    First version of lab 3_5

--- LAB3_5SpamChecker/Program.cs
+++ LAB3_5SpamChecker/Program.cs
@@ -6,7 +6,21 @@ namespace LAB3_5SpamChecker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a message.");
+            string blackListWord = "Viagra";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                Console.WriteLine("The message is not spam.");
+
+            }
         }
     }
 }
